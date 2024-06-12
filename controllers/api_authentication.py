8bea5c4795149f9dfb4f70d59b1a77c4f4259491
Tito Carlos <titocarlos080@gmail.com>
2024-06-12 13:56:39 -0400

    auth

--- controllers/api_authentication.py
+++ controllers/api_authentication.py
@@ -13,32 +13,11 @@ class AuthenticationController(http.Controller):
         if not username or not password:
             return request.make_response(json.dumps({'error': 'Nombre de usuario y contraseña requeridos'}), status=400)
 
-        user = request.env['res.users'].sudo().search([('login', '=', username)], limit=1)
-        if not user or not user._check_credentials(password):
-            return request.make_response(json.dumps({'error': 'Credenciales inválidas'}), status=401)
-
+        # Buscar el usuario por correo electrónico
+        user = request.env['res.partner'].sudo().search([('email', '=', username)], limit=1)
         # Generar un token único
         token = str(uuid.uuid4())
+         # Actualizar el token en el usuario
+        user.write({'keynotificaciones': token})
 
-        # Almacenar el token en la base de datos
-        auth_token = request.env['auth.token'].sudo().create({
-            'user_id': user.id,
-            'token': token
-        })
-
-        return request.make_response(json.dumps({'token': token}), headers={'Content-Type': 'application/json'})
-
-    @http.route('/odoo/auth/logout', auth='public', methods=['POST'], csrf=False)
-    def logout(self, **kwargs):
-        token = kwargs.get('token')
-
-        if not token:
-            return request.make_response(json.dumps({'error': 'Token de autenticación requerido'}), status=400)
-
-        auth_token = request.env['auth.token'].sudo().search([('token', '=', token)], limit=1)
-        if not auth_token:
-            return request.make_response(json.dumps({'error': 'Token de autenticación inválido'}), status=401)
-
-        auth_token.unlink()
-
-        return request.make_response(json.dumps({'message': 'Sesión cerrada exitosamente'}), headers={'Content-Type': 'application/json'})
+        return request.make_response(json.dumps({'token': token, 'user': user.read()}), headers={'Content-Type': 'application/json'})
