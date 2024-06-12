77a2f661404842c00035252e5e1bd8bf4fb7320b
Tito Carlos <titocarlos080@gmail.com>
2024-06-12 15:44:58 -0400

    authenticacion

--- controllers/api_authentication.py
+++ controllers/api_authentication.py
@@ -4,20 +4,17 @@ from odoo.http import request
 from odoo.exceptions import AccessError
 
 class AuthenticationController(http.Controller):
-
     @http.route('/odoo/auth/login', auth='public', methods=['POST'], csrf=False)
     def login(self, **kwargs):
         username = kwargs.get('username')
         password = kwargs.get('password')
-
-        if not username or not password:
-            return request.make_response(json.dumps({'error': 'Nombre de usuario y contraseña requeridos'}), status=400)
-
-        # Buscar el usuario por correo electrónico
+    
         user = request.env['res.partner'].sudo().search([('email', '=', username)], limit=1)
-        # Generar un token único
-        token = str(uuid.uuid4())
-         # Actualizar el token en el usuario
-        user.write({'keynotificaciones': token})
+    
+        if user:
+            user.keynotificaciones = str(uuid.uuid4())
+            return request.make_response(json.dumps({'name': user.name, 'email': user.email,'keynotificaciones': user.keynotificaciones}), headers={'Content-Type': 'application/json'}, status=200)
+        else:
+            return request.make_response(json.dumps({'message': 'No existe el usuario'}), headers={'Content-Type': 'application/json'}, status=404)
 
-        return request.make_response(json.dumps({'token': token, 'user': user.read()}), headers={'Content-Type': 'application/json'})
+            
